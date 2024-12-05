854d2efc7ad53d4feabc0f8d6ba34a10efea5184
Gerald <geraldeburkholder@gmail.com>
2024-12-05 01:42:30 -0600

    starting day2

---
+++ 2024/day_2/index.js
@@ -0,0 +1,15 @@
+/* 
+  Red-Nosed Reports 
+  https://adventofcode.com/2024/day/2
+*/
+
+const fs = require('fs');
+const { performance } = require('perf_hooks');
+
+const data = fs.readFileSync('input.txt', 'utf8');
+
+const lines = data.split('\n');
+
+lines.forEach(line => {
+    // do something
+});
