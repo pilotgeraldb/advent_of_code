529b9e5fbc0e769eeed027920f577b63b631f023
geraldb88 <geraldeburkholder@gmai.com>
2025-12-03 10:31:31 -0600

    add main test for day_2

--- 2025/day_2/go.mod
+++ 2025/day_2/go.mod
@@ -1,3 +1,5 @@
 module day_2_aoc_2025
 
 go 1.25.1
+
+require github.com/stretchr/testify v1.11.1 // indirect
---
+++ 2025/day_2/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+	assert.Equal(t, 0, 0, "they should be equal")
+}
