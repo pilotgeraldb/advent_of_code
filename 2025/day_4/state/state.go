9c1c1e0f7e251594736bfbe3f9fe1b298988065b
geraldb88 <geraldeburkholder@gmai.com>
2025-12-18 14:54:45 -0600

    part 2

--- 2025/day_4/state/state.go
+++ 2025/day_4/state/state.go
@@ -6,6 +6,7 @@ type State struct {
 	arr         []bool
 	rowLength   int
 	maxAdjacent int
+	removed     []bool
 }
 
 func NewState(rowLength int, maxAdjacent int) *State {
@@ -25,23 +26,38 @@ func (s *State) Value() *[]bool {
 }
 
 func (s State) Evaluate() int {
-	total := 0
 	arr := *s.Value()
-	for i, v := range arr {
-		if v {
-			a := s.Adjacent(i)
-			c := a.Count()
-			if c <= s.maxAdjacent {
-				log.Printf("counting %d because c = %d", i+1, c)
-				total++
-			} else {
-				log.Printf("not counting %d because c = %d", i+1, c)
+	s.removed = make([]bool, len(arr))
+	max := 5_000_000
+	iteration := 0
+	total := 0
+	for {
+		hasRemoval := false
+		for i, v := range arr {
+			if s.removed[i] {
+				continue
 			}
-		} else {
-			log.Printf("not counting %d because value is false", i+1)
+			if v {
+				a := s.Adjacent(i)
+				c := a.Count()
+				if c <= s.maxAdjacent {
+					//add to removal index
+					s.removed[i] = true
+					hasRemoval = true
+					total++
+				}
+			}
+		}
+
+		if !hasRemoval {
+			return total
+		}
+
+		iteration++
+		if iteration > max {
+			log.Fatalf("exceeded max iterations of %d", max)
 		}
 	}
-	return total
 }
 
 func (s State) Adjacent(idx int) AdjacentResult {
@@ -49,35 +65,51 @@ func (s State) Adjacent(idx int) AdjacentResult {
 
 	//north
 	if idx-s.rowLength >= 0 {
-		result.North = s.arr[idx-s.rowLength]
+		if !s.removed[idx-s.rowLength] {
+			result.North = s.arr[idx-s.rowLength]
+		}
 	}
 	//south
-	if idx+s.rowLength < len(s.arr) {
-		result.South = s.arr[idx+s.rowLength]
+	if idx+s.rowLength < len(s.arr) && idx+s.rowLength < len(s.removed) {
+		if !s.removed[idx+s.rowLength] {
+			result.South = s.arr[idx+s.rowLength]
+		}
 	}
 	//west
 	if idx%s.rowLength != 0 {
-		result.West = s.arr[idx-1]
+		if !s.removed[idx-1] {
+			result.West = s.arr[idx-1]
+		}
 	}
 	//east
 	if (idx+1)%s.rowLength != 0 {
-		result.East = s.arr[idx+1]
+		if !s.removed[idx+1] {
+			result.East = s.arr[idx+1]
+		}
 	}
 	//northwest
 	if idx-s.rowLength >= 0 && idx%s.rowLength != 0 {
-		result.NorthWest = s.arr[idx-s.rowLength-1]
+		if !s.removed[idx-s.rowLength-1] {
+			result.NorthWest = s.arr[idx-s.rowLength-1]
+		}
 	}
 	//northeast
 	if idx-s.rowLength >= 0 && (idx+1)%s.rowLength != 0 {
-		result.NorthEast = s.arr[idx-s.rowLength+1]
+		if !s.removed[idx-s.rowLength+1] {
+			result.NorthEast = s.arr[idx-s.rowLength+1]
+		}
 	}
 	//southwest
 	if idx+s.rowLength < len(s.arr) && idx%s.rowLength != 0 {
-		result.SouthWest = s.arr[idx+s.rowLength-1]
+		if !s.removed[idx+s.rowLength-1] {
+			result.SouthWest = s.arr[idx+s.rowLength-1]
+		}
 	}
 	//southeast
 	if idx+s.rowLength < len(s.arr) && (idx+1)%s.rowLength != 0 {
-		result.SouthEast = s.arr[idx+s.rowLength+1]
+		if !s.removed[idx+s.rowLength+1] {
+			result.SouthEast = s.arr[idx+s.rowLength+1]
+		}
 	}
 
 	return result
